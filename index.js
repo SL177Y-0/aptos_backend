0d80fe7be44786d24674621ad87873d2b177186f
RISHI RAWAT <sl177y.log0@gmail.com>
2025-09-21 19:31:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function fixMoveToml(moveTomlContent) {
         if (!tomlContent.includes("[dependencies]")) {
             tomlContent += "\n\n[dependencies]";
         }
-        tomlContent += "\nAptosFramework = { git = \"https://github.com/aptos-labs/aptos-core.git\", subdir = \"aptos-move/framework/aptos-framework\", rev = \"aptos-cli-v2.4.0\" }";
+        tomlContent += "\nAptosFramework = { git = \"https://github.com/aptos-labs/aptos-core.git\", subdir = \"aptos-move/framework/aptos-framework\", rev = \"aptos-node-v1.34.2\" }";
     }
     
     // Replace {{ADDR}} with actual account address
